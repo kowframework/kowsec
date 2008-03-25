ad5a0e18f4b309c3e5b82278cd2b69a770d60817
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2008-03-25 19:00:04 +0000

    adding base for awsec
    git-svn-id: http://dev.adaworks.net/svn/trunk/awsec@287 115cb82a-aa27-0410-86a0-a2651309cc67

---
+++ src/aw_sec.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO;	use Ada.Text_IO;
+
+package body Aw_Sec is
+
+
+begin
+	Put_Line("bu!");
+end Aw_Sec;
---
+++ src/aw_sec.ads
@@ -0,0 +1,8 @@
+
+
+
+package Aw_Sec is
+
+	pragma Elaborate_Body;
+
+end Aw_Sec;
