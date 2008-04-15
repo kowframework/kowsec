65333cfff12b52099874ee38d40b380c86dc272a
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2008-04-15 00:22:24 +0000

    the user and authentication management design has been stablished. Now it's time to implement the base code and some backends
    git-svn-id: http://dev.adaworks.net/svn/trunk/awsec@328 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/aw_sec.adb
+++ src/aw_sec.adb
@@ -1,19 +1,92 @@
-with Ada.Text_IO;	use Ada.Text_IO;
 
 package body Aw_Sec is
-	function Is_Anonymous( U: in User ) return Boolean is
+
+	procedure Check_Anonymous_Access( User_Object: in User; Where: in String ) is
 	begin
-		return U.Is_Anonymous;
+		if Is_Anonymous( User_Object ) then
+			raise ANONYMOUS_ACCESS
+				with 
+					"Can't get information from anonymous user [" &
+					Where &
+					"]";
+		end if;
+	end Check_Anonymous_Access;
+
+	procedure Update_Groups( User_Object: in out User ) is
+		-- Tells the user that his groups should be updated in the
+		-- next request to Get_Groups.
+	begin
+		User_Object.Groups_Cache.Set_Update;
+	end Update_Groups;
+
+
+	function Identity( User_Object: in User ) return String is
+		-- Return a string identifying the current user. Usually it's the username
+		-- but one could implement other methods, such as a numeric id for this user
+	begin
+		return To_String( User_Object.Username );
+	end Identity;
+
+
+	function Full_Name(	User_Object	: in User;
+				Locale		: Aw_Lib.Locales.Locale
+					:= Aw_Lib.Locales.Default_Locale
+		) return String is
+		-- return the full name for this user, respecting the locale's conventions
+	begin
+		Check_Anonymous_Access( User_Object, "Full_Name" );
+		return Aw_Lib.Locales.Format_Full_Name(
+			Locale		=> Locale,
+			First_Name	=> To_String( First_Name ),
+			Last_Name	=> To_String( Last_Name )
+			);
+	end Full_Name;
+
+	
+	function Get_Groups( User_object: in User'Class ) return Authorization_Groups is
+	-- Get the groups for this user.
+	-- There are two things to notice here:
+	-- 	1. This method is task safe. It means it will never return something
+	--	 while the user's group list is being generated.
+	--	2. The user's group is auto-updated by using the methods:
+	--		. Set_Groups_Timeout( User, Duration);
+	--		. Update_Groups( User );
+
+
+	begin
+		Check_Anonymous_Access( User_Object, "Get_Groups" );
+		return User_Object.Groups_Cache.Get_Groups;
+	end Get_Groups;
+
+	function Is_Anonymous(	User_Object: in User ) return Boolean is
+	-- Return true if this user isn't logged in.
+	-- 
+	-- Even though this isn't an abstract method, one can overwrite it
+	-- in order to log hits from anonymuos users when using determined Manager.
+		Username: String := To_String( User_Object.Username );
+	begin
+		if Username'Length = 0  OR Username = Anonymous_Username then
+			return true;
+		else
+			return false;
+		end if;
 	end Is_Anonymous;
 
-
-
-	procedure Adjust( U : in out User ) is
+	procedure Do_Logout( User_Object: in out User ) is
+	-- Not only make sure the user is logged out but also
+	-- Make sure Is_Anonymous returns true for now on for this user.
+		Null_String := To_Unbounded_String( "" );
 	begin
-		U.Is_Anonymous := False;
-	end Adjust;
+		User.Username := Anonymous_Username;
+		User.First_Name := Null_Unbounded_String;
+		User.Last_Name := Null_Unbounded_String;
 
+		-- there is no need to clear the user's cache
+		-- as Get_Groups always checks if it's an anonymous user or not.
+		--
+		--
+		-- TODO: implement something to clear this cache in order to
+		-- recycle the memory
+	end Do_Logout;
 
-begin
-	Put_Line("bu!");
 end Aw_Sec;
--- src/aw_sec.ads
+++ src/aw_sec.ads
@@ -36,7 +36,17 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
+
+-- TODO:
+-- 	. authorization
+-- 	. accounting
+--
+-- Notice:
+-- 	I decided to finish the user and authentication schema before continuing
+-- 	the rest of Aw_Sec design as it's a critical area.
+-- 		OgRo.
 with Ada.Finalization;
+with Ada.Strings.Unbounded;	use Ada.Strings.Unbounded;
 
 package Aw_Sec is
 
@@ -51,26 +61,23 @@ package Aw_Sec is
 	-- Groups Management --
 	-----------------------
 	
-	type Authorization_Group is tagged private;
+	type Authorization_Group is new Unbounded_String; 
 	-- Represents an authorization group for this user.
 	-- Even though the main concept of groups is the unix meaning of groups
 	-- this group can represent some ACL assigned to the user.
 	--
 	-- That's the reason why we call it Authorization_Group and not only Group.
+	--
+	-- It's an Unbounded_String type because there is no way to predict the
+	-- string size in a Authorization_Manager and the groups can be retrieved
+	-- from different managers for the same user (see the rest of the file)
 
 
-	function Identity( Group: in Authorization_Group ) return String;
-	-- return a string identifying the current group;
-	-- this can be the group name, for instance.
+	type Authorization_Groups is Array(<>) of Authorization_Group;
+	-- an array containing authorization groups.
 
 
-
-	type Authorization_Group_Access is access all Authorization_Group'Class;
-
-	type Authorization_Groups is Array(<>) of Authorization_Group_Access;
-	-- an array containing access types for authorization groups.
-
-
+	-- there is no special method for the Authorization_Group type.
 
 	---------------------
 	-- User Management --
@@ -79,14 +86,18 @@ package Aw_Sec is
 	type User is tagged private;
 	-- A user can be extended, even though it's not how Aw_Sec should be 
 	-- extended
+	--
 	-- Instead, try extending the Authentication_Manager type.
+	--
+	-- Here, the user is a read-only type, meaning there is no implementation
+	-- of methods for changing user's properties.
+	--
+	-- This should be handler somewhere else.
 
-	function Is_Anonymous(	User_Object: in User ) return Boolean;
-	-- Return true if this user isn't logged in.
-	-- 
-	-- Even though this isn't an abstract method, one can overwrite it
-	-- in order to log hits from anonymuos users when using determined Manager.
-
+	
+	procedure Update_Groups( User_Object: in out User );
+	-- Tells the user that his groups should be updated in the
+	-- next request to Get_Groups.
 
 
 	function Identity( User_Object: in User ) return String;
@@ -98,21 +109,36 @@ package Aw_Sec is
 				Locale		: Aw_Lib.Locales.Locale
 					:= Aw_Lib.Locales.Default_Locale
 		) return String;
-	-- return the full name for this user, respecting the locale
-
-
-	function Clear_Data( User_Object: in out User );
-	-- clear all the data implemented in the extending type
-	-- of the current user 
-
-	procedure Do_Logout( User_Object: in out User'Class );
-	-- Not only make sure the user is logged out but also
-	-- Make sure Is_Anonymous returns true for now on for this user.
-	-- 
-	-- Even though it could sound strange that the login is handled by a
-	-- manager and the logout is managed by the logout.
+	-- return the full name for this user, respecting the locale's conventions
 
 	
+	function Get_Groups( User_object: in User'Class ) return Authorization_Groups;
+	-- Get the groups for this user.
+	-- There are two things to notice here:
+	-- 	1. This method is task safe. It means it will never return something
+	--	 while the user's group list is being generated.
+	--	2. The user's group is auto-updated by using the methods:
+	--		. Set_Groups_Timeout( User, Duration);
+	--		. Update_Groups( User );
+
+
+
+	function Is_Anonymous(	User_Object: in User ) return Boolean;
+	-- Return true if this user isn't logged in.
+	-- 
+	-- Even though this isn't an abstract method, one can overwrite it
+	-- in order to log hits from anonymuos users when using determined Manager.
+
+
+	procedure Do_Logout( User_Object: in out User );
+	-- Not only make sure the user is logged out but also
+	-- Make sure Is_Anonymous returns true for now on for this user.
+
+
+	-------------------------------
+	-- AUTHENTICATION MANAGEMENT --
+	-------------------------------
+
 	type Authentication_Manager is abstract new Ada.Finalization.Controlled with private;
 	-- This is where the magic happens!
 	--
@@ -126,21 +152,16 @@ package Aw_Sec is
 				Username: in String;
 				Password: in String ) return User'Class is abstract;
 	-- Login the user, returning a object representing it.
-	-- This object might be a direct instance of User or a subclas.
+	-- This object might be a direct instance of User or a subclass.
 	-- It's this way so the authentication method might have
 	-- a user with extended properties.
 
 
-	function Get_Groups(	Manager:	in Authentication_Manager;
-				User_Object:	in User'Class ) return Authorization_Groups;
-	-- Return all the groups for this user
-	-- It's implemented in the manager for 2 reasons:
-	-- 	1. this way we can store the users and the groups in
-	-- 	  different managers.
-	-- 	2. the information on how to obtain the groups information
-	-- 	  doesn't belong to the user itself.
 
-	function Get_Groups( User_object: in User'Class ) return Authorization_Groups;
+	----------------------------------------
+	-- USER AND AUTHENTICATION EXCEPTIONS --
+	----------------------------------------
+
 
 	INVALID_CREDENTIALS: Exception;
 	-- should be raised when login fails.
@@ -148,6 +169,11 @@ package Aw_Sec is
 	ANONYMOUS_ACCESS: Exception;
 	-- should be raised when trying to get information from an anonymous user.
 
+
+
+	-- TODO: FROM HERE!
+	
+	
 	------------------------------
 	-- Authorization Management --
 	------------------------------
@@ -176,6 +202,7 @@ package Aw_Sec is
 
 	INVALID_CRITERIA: Exception;
 	-- raised when trying to create or unload an unknown criteria.
+
 	DUPLICATED_CRITERIA: Exception;
 	-- raised when trying to register another criteria with the same name.
 
@@ -400,36 +427,76 @@ private
 
 
 	protected type Groups_Cache_Type is 
+		function Should_Update return Boolean;
+		-- Determines if the this cache should be update.
+		-- The criteria for this is:
+		-- 	if Authorization_Groups = NULL then
+		-- 		return_code := true;
+		-- 	else if Need_Update = True then 
+		-- 		return_code := true;
+		-- 	else if Timeout /= 0.0 and then Last_Update < Now - Timeout then
+		-- 		return_code := true;
+		-- 	else 
+		-- 		return_code := false;
+		-- 	end if;
+		--
+		-- 	return return_code;
+		
+		
+		procedure Update( Managers: in Authorization_Managers );
+		-- update the groups and then set:
+		-- 	need_update := false
+		-- 	last_update := now
 
+		function Get_Groups return Authorization_Groups;
+		-- checks if the groups should be update
+		-- 	if true, do the update
+		-- 	if false, don't update.
+		-- return the current groups list
+
+		procedure Set_Update;
+		-- tell this cache it should be updated in the next call
+		-- of Get_Groups.
 	private
 		Groups		: access Authorization_Groups;
 		Timeout		: Duration := 600.0;		-- the duration of this cache in secconds
 		Need_Update	: Boolean := True;		-- should update the cache in the next Get_Groups call?
-		Last_Access	: Time;				-- when was the last access to this information.
-	end record;
-
-	function Should_Update( Cache: in Groups_Cache_Type ) return Boolean;
-	-- se Need_Update = true, define need_update := false e retorna true
-	-- se agora - last_access >= timeout, retorna true
-	-- return false
-	
-	procedure Update(	Cache: in out Groups_Cache_Type;
-				Managers: Authorization_Managers );
-	-- update the current cache
+		Last_Update	: Time;				-- when was the last access to this information.
+	end Groups_Cache_Type;
 
 
+	Anonymous_Username : Unbounded_String := To_Unbounded_String( "anonymous" );
 
 	type User is tagged record
-		Username	: access String;
-
-		Groups_Cache	: access Authorization_Groups;
-		Managers	: access Authorization_Manager_Vectors.Vector;
-		Groups_Cache_Update:
+		Username	: Unbounded_String := Anonymous_Username;
+		-- as default, in Aw_Sec, anonymous user has this username
+		First_Name	: Unbounded_String;
+		Last_Name	: Unbounded_String;
+		-- this one is optional, depending on the Authorization_Manager
+		Groups_Cache	: Groups_Cache_Type;
+		Managers	: Authorization_Manager_Vectors.Vector;
 	end record;
 
 	-- TODO: from here and package body!
-	type Authentication_Manager is abstract bew Ada.Finalization.Controlled with record
+	type Authentication_Manager is abstract new Ada.Finalization.Controlled with record
 	end record;
+
+	function Get_Groups(	Manager:	in Authentication_Manager;
+				User_Object:	in User'Class )
+				return Authorization_Groups is abstract;
+	-- Return all the groups for this user
+	-- It's implemented in the manager for 2 reasons:
+	-- 	1. this way we can store the users and the groups in
+	-- 	  different managers.
+	-- 	2. the information on how to obtain the groups information
+	-- 	  doesn't belong to the user itself.
+	-- When implementor of this method should assume:
+	-- 	1. the user is valid and so is the results of Identity( User_Object );
+	-- 	2. it's meant to work with any authentication manager vs user combination.
+	-- This is a private method so the user won't call it directly.
+	-- Instead, it's called by the Get_Groups (User'Class) method implemented here.
+
+
 
 	type Accountant is new Ada.Finalization.Controlled with record
 	end record;
