326d44c03d775cd5ae60f3166598b1ef11e1d9c9
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2011-01-03 13:24:10 -0200

    fixed elaboration circularity

--- src/kow_sec.ads
+++ src/kow_sec.ads
@@ -41,6 +41,7 @@ with Ada.Calendar;			use Ada.Calendar;
 with Ada.Containers.Vectors;
 with Ada.Containers.Ordered_Maps;
 with Ada.Finalization;
+with Ada.Sequential_IO;
 with Ada.Strings.Unbounded;		use Ada.Strings.Unbounded;
 with Ada.Containers.Hashed_Maps;
 with Ada.Strings.Unbounded.Hash;
@@ -62,6 +63,135 @@ package KOW_Sec is
 	Storage_Root : constant String := Get_Working_Dir / "appdata" / "kow_sec";
 	-- where the KOW_Sec.Data will store their files
 	
+
+
+	------------------
+	-- Data Storage --
+	------------------
+
+	generic
+		Storage_Name : String;
+		type Key_Type is private;
+		with function To_String( Key : in Key_Type ) return String;
+		with function "<"( L,R : in Key_Type ) return Boolean;
+		type Element_Type is private;
+		with package Element_Vectors is new Ada.Containers.Vectors(
+						Index_Type	=> Positive,
+						Element_Type	=> Element_Type
+					);
+		--Cache_Size	: Natural;	-- elements in cache
+		--Index_Data	: Boolean;	-- should the data be indexed?
+	package Data is
+
+
+		------------------
+		-- Storage Path --
+		------------------
+		Storage_Root : constant String := KOW_Sec.Storage_Root / Storage_Name;
+
+
+		function Storage_Path( Key : in Key_Type ) return String;
+
+
+		---------------------
+		-- In-memory cache --
+		---------------------
+
+		package Cache_Maps is new Ada.Containers.Ordered_Maps(
+					Key_type	=> Key_Type,
+					Element_Type	=> Element_Vectors.Vector,
+					"="		=> Element_Vectors."="
+				);
+
+		protected Cache is
+			-- it's a infinite cache for every element loaded so far...
+			-- to avoid colisions we are using ordered maps.. the ideal is to use an
+			-- ordered map with a hash function with no colisions at all tough
+
+			procedure Read(
+						Key		: in     Key_Type;
+						Item		:    out Element_Vectors.Vector;
+						From_Disk	: in     Boolean := False
+					);
+			-- tries reading from cache.. if not cache, read file...
+			-- if from_disk = true reads from the file anyway
+
+			procedure Write(
+						Key	: in     Key_Type;
+						Item	: in     Element_Vectors.Vector
+					);
+			-- update cache with new value
+			-- if on_disk = true then update the disk file also
+
+			procedure Append(
+						Key	: in     Key_Type;
+						Item	: in     Element_Type
+					);
+			-- append a new item into the storage
+			-- if on_disk = true then update the disk also
+
+			procedure Delete(
+						Key		: in     Key_Type;
+						From_Disk	: in     Boolean := False
+					);
+			-- delete from map and, if required, from disk also
+		private
+			Cache_Map : Cache_Maps.Map;
+		end Cache;
+
+
+		-------------
+		-- Aliases --
+		-------------
+
+		procedure Append(
+					Key	: in Key_Type;
+					Element	: in Element_Type
+				) renames Cache.Append;
+
+
+		procedure Store(
+					Key	: in Key_Type;
+					Elements: in Element_Vectors.Vector 
+				) renames Cache.Write;
+		
+		procedure Delete(
+					Key		: in Key_type;
+					From_Disk	: in Boolean := False
+				) renames Cache.Delete;
+
+		---------------------
+		-- Other functions --
+		---------------------
+
+		function Get_First(
+					Key	: in Key_Type;
+					Unique	: in Boolean := False
+				) return Element_Type;
+		-- get only the first element..
+		-- if unique and it's not the only element then raise constraint_error
+		-- if there is no element raise constraint_error also
+		
+		function Get_All( Key : in Key_Type ) return Element_Vectors.Vector;
+		-- get all elements
+
+
+		procedure Store(
+					Key	: in Key_Type;
+					Element	: in Element_Type
+				);
+		-- store the element making it the only one in the file
+
+	private
+		package Element_IO is new Ada.Sequential_IO( Element_Type );
+
+	end Data;
+
+
+
+
+
+
 	----------------------------
 	-- The User Identity Type --
 	----------------------------
@@ -456,5 +586,6 @@ package KOW_Sec is
 
 	ACCESS_DENIED: Exception;
 	-- I think the name is clear enough...
+
 
 end KOW_Sec;
